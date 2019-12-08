51c067780552624e535c02002d813d7e309d4580
Jayson Virissimo <jayson.virissimo@asu.edu>
2019-12-07 19:34:29 -0700

    Easily add questions to reading from index

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -28,7 +28,7 @@ class QuestionsController < ApplicationController
 
     respond_to do |format|
       if @question.save
-        format.html { redirect_to @question, notice: 'Question was successfully created.' }
+        format.html { redirect_to readings_url, notice: 'Question was successfully created.' }
         format.json { render :show, status: :created, location: @question }
       else
         format.html { render :new }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
+  root to: 'readings#index'
   resources :questions
   resources :readings
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
