258868f324353468e435a2e7d01f5c7a13697194
Jayson Virissimo <jayson.virissimo@asu.edu>
2020-01-03 21:44:54 -0700

    Deprecate comment concept

--- app/models/comment.rb
+++
@@ -1,6 +0,0 @@
-# frozen_string_literal: true
-
-class Comment < ApplicationRecord
-  belongs_to :reading
-  validates :body, allow_blank: false, presence: true, uniqueness: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   resources :reactions
-  resources :comments
   root to: 'readings#index'
   resources :readings
 end
