0ee23c2a3444bfece2d2119b51d5e0136e8bfc29
Jayson Virissimo <jayson.virissimo@asu.edu>
2020-01-17 20:51:06 -0700

    Rename the app
    Don't try to be too general; fix the specific problem and nothing more

--- config/application.rb
+++ config/application.rb
@@ -19,7 +19,7 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module BookClub
+module ScriptureGroup
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
