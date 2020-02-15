e2b7aab448dea9b9519d5d96ddb167e33f8cbf8b
Jayson Virissimo <jayson.virissimo@asu.edu>
2020-02-15 10:22:11 -0700

    Sections link back to the reading

--- app/helpers/sections_helper.rb
+++ app/helpers/sections_helper.rb
@@ -1,5 +1,15 @@
+# frozen_string_literal: true
+
 module SectionsHelper
+  def back_to(reading)
+    link_to("Back to the reading #{link_svg}".html_safe, reading)
+  end
+
+  def link_svg
+    Octicons::Octicon.new('link').to_svg.html_safe
+  end
+
   def pencil_svg
-    Octicons::Octicon.new("pencil").to_svg.html_safe
+    Octicons::Octicon.new('pencil').to_svg.html_safe
   end
 end
