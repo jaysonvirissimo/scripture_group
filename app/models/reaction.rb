3144ee710db4b7dc2bc705ba2cb3aa0a20d93975
Jayson Virissimo <jayson.virissimo@asu.edu>
2020-02-15 14:51:51 -0700

    Order reactions by section

--- app/models/reaction.rb
+++ app/models/reaction.rb
@@ -10,6 +10,12 @@ class Reaction < ApplicationRecord
 
   validates :body, presence: true
 
+  def self.ordered_by_section
+    includes(:section).sort_by do |reaction|
+      reaction.section&.created_at || Time.current
+    end
+  end
+
   def link_to_anchored_verse
     link_to(verse.title, section.path_to(verse))
   end
